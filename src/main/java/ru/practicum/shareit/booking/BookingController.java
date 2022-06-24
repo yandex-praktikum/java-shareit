c48c724b7f3171bf04f893482b07e93b37d3a938
Vladimir Ivanov <ivanov.vladimir.l@gmail.com>
2022-06-24 10:41:32 +0300

    Init

---
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -0,0 +1,12 @@
+package ru.practicum.shareit.booking;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * // TODO .
+ */
+@RestController
+@RequestMapping(path = "/bookings")
+public class BookingController {
+}
---
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -0,0 +1,12 @@
+package ru.practicum.shareit.item;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * // TODO .
+ */
+@RestController
+@RequestMapping("/items")
+public class ItemController {
+}
