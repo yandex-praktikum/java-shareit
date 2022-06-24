41780ce01744bc0d44a7527839d683363f019ac7
Vladimir Ivanov <ivanov.vladimir.l@gmail.com>
2022-06-24 10:41:32 +0300

    Init

---
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -0,0 +1,13 @@
+package ru.practicum.shareit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareItTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
