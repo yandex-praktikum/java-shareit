03ae475b029830f7c63724b58e6ecc03ed6c1bf7
EAnchugov <bergy@yandex.ru>
2022-10-31 04:08:44 +0300

    update tests is OK

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepositoryImpl.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepositoryImpl.java
@@ -8,18 +8,18 @@ import java.util.Map;
 @Repository
 public class ItemRepositoryImpl implements ItemRepository{
     private Map<Long, Item> itemMap = new HashMap<>();
-    private long tmpId = 0;
+    private Long tmpId = 0L;
     @Override
     public Item create(Item item) {
         tmpId++;
         item.setId(tmpId);
         itemMap.put(tmpId,item);
         return item;
+
     }
     @Override
     public void update(Item item) {
-        itemMap.put(item.getId(), item);
-
+        itemMap.replace(item.getId(), item);
     }
     @Override
     public Item getById(Long itemID){
--- src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -2,7 +2,9 @@ package ru.practicum.shareit.item.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.practicum.shareit.exceptions.DuplicateEmailException;
 import ru.practicum.shareit.exceptions.NotFoundException;
+import ru.practicum.shareit.exceptions.WrongParameterException;
 import ru.practicum.shareit.item.itemDto.ItemDto;
 import ru.practicum.shareit.item.itemDto.ItemMapper;
 import ru.practicum.shareit.item.model.Item;
@@ -17,26 +19,37 @@ public class ItemServiceImpl implements ItemService{
     private final ItemMapper itemMapper;
     @Override
     public ItemDto createItem(ItemDto itemDto, Long userId) {
-        checkUserAvailability(userId);
-        return itemMapper.toItemDto(itemRepository.create(itemMapper.toItem(itemDto)));
+        checkUser(userId);
+
+        ItemDto itemDto1 = itemDto;
+        itemDto1.setOwner(userId);
+        return itemMapper.toItemDto(itemRepository.create(itemMapper.toItem(itemDto1)));
     }
 
     @Override
     public ItemDto updateItem(ItemDto itemDto, Long userId, Long itemId) {
+        checkUser(userId);
         Item item1 = itemRepository.getById(itemId);
-        if (itemDto.getAvailable() !=null){item1.setAvailable(itemDto.getAvailable());}
-        if (itemDto.getName() != null){item1.setName(itemDto.getName());}
-        if (itemDto.getDescription() != null){item1.setDescription(itemDto.getDescription());}
-        if (itemDto.getOwner() !=null){item1.setOwner(itemDto.getOwner());}
-        if (itemDto.getRequest() !=null){item1.setRequest(itemDto.getRequest());}
-        itemRepository.update(item1);
-        return itemMapper.toItemDto(item1);
+        if (item1 != null){
+            if (!(item1.getOwner().equals(userId))) {throw new NotFoundException("Изменять может только владелец");}
+            if (itemDto.getAvailable() !=null){item1.setAvailable(itemDto.getAvailable());}
+            if (itemDto.getName() != null){item1.setName(itemDto.getName());}
+            if (itemDto.getDescription() != null){item1.setDescription(itemDto.getDescription());}
+            if (itemDto.getOwner() !=null){item1.setOwner(itemDto.getOwner());}
+            if (itemDto.getRequest() !=null){item1.setRequest(itemDto.getRequest());}
+            itemRepository.update(item1);
+            return itemMapper.toItemDto(item1);
+        }
+        else {
+            throw  new WrongParameterException("Вещь не найдена");
+        }
+
     }
-    private void checkUserAvailability (Long userId){
+
+    private void checkUser(Long userId){
         try{
             userService.getById(userId);}
         catch (RuntimeException e){
             throw new NotFoundException("Юзер с ID " + userId + " не найден");}
-
     }
 }
