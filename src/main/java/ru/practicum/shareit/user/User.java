bc6fc62894753eb38c7fa1ae458b61b8a3f8021f
Anton <antnbrnv@yandex.ru>
2023-01-13 13:59:52 +0300

    feat: create class User

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,18 @@
 package ru.practicum.shareit.user;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
 /**
  * TODO Sprint add-controllers.
  */
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
 public class User {
+    Long userId;
 }
