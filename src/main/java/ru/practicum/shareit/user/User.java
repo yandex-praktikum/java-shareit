a4a29322482a92c037bf86eac8da748a754101d6
Andersen <sergey@malyuga.ru>
2023-10-23 19:23:40 +0300

    refactor: создать class User

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,18 @@
 package ru.practicum.shareit.user;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import javax.validation.constraints.Email;
+
+@Data
+@EqualsAndHashCode
+@ToString
 public class User {
+
+    private int id;
+    private String name;
+    @Email(message = "Не правильный формат email")
+    private String email;
 }
