653b7e2f9f5766de710d6174d5b208207eb574ff
Andersen <sergey@malyuga.ru>
2023-10-24 19:54:41 +0300

    refactor: создать class BookingDto, class ItemDto, class ItemRequestDto, class UserDto

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,7 +1,23 @@
 package ru.practicum.shareit.booking.dto;
 
-/**
- * TODO Sprint add-bookings.
- */
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import ru.practicum.shareit.booking.BookingStatus;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.User;
+
+import java.time.LocalDateTime;
+
+@Data
+@EqualsAndHashCode
+@ToString
 public class BookingDto {
+
+    private int id;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private Item item;
+    private User booker;
+    private BookingStatus status;
 }
---
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -0,0 +1,25 @@
+package ru.practicum.shareit.user.dto;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+import org.springframework.stereotype.Component;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Accessors(chain = true)
+@EqualsAndHashCode
+@ToString
+@Component
+public class UserDto {
+
+    private int id;
+    @NotBlank(message = "Name - не может быть пустым.")
+    private String name;
+    @NotBlank(message = "Email - не может быть пустым.")
+    @Email(message = "Не правильный формат email")
+    private String email;
+}
